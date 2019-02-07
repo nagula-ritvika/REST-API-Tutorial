82741292b89150069768ae08e4c68f4b0d8c4b8a
nagula-ritvika <nagula.ri@husky.neu.edu>
2019-02-06 20:01:09 -0500

    added mongodb too

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 #__author__ = ritvikareddy2
 #__date__ = 2019-02-06
 
+import json
+from bson.json_util import dumps
 from flask import Flask, render_template
 from flask_restful import Api, Resource, reqparse
+from pymongo import MongoClient
 
-import json
 
 app = Flask(__name__)
 api = Api(app)
+client = MongoClient('localhost', 27017)
+db = client['sample_db']
+users_collection = db.users
 
 
 @app.route('/')
@@ -16,10 +21,12 @@ def home():
 
 
 def load_data():
+    users_collection.drop()
     with open('users.json') as f:
         data = json.load(f)
+    result = users_collection.insert_many(data)
     # print(data)
-    return data
+    return result
 
 
 class User(Resource):
@@ -27,9 +34,9 @@ class User(Resource):
     users = load_data()
 
     def get(self, name):
-        for user in self.users:
-            if name == user['name']:
-                return user, 200
+        res = users_collection.find_one({'name': name}, {"_id": 0})
+        if res:
+            return res, 200
         return 'User doesnt exist', 404
 
     def post(self, name):
@@ -38,9 +45,9 @@ class User(Resource):
         parser.add_argument("occupation")
         args = parser.parse_args()
 
-        for user in self.users:
-            if name == user['name']:
-                return 'User already exists', 400
+        res = users_collection.find_one({'name': name})
+        if res:
+            return 'User already exists', 400
 
         user = {
             'name': name,
@@ -48,8 +55,8 @@ class User(Resource):
             'occupation': args['occupation']
         }
 
-        self.users.append(user)
-
+        users_collection.insert_one(user)
+        user.pop('_id')
         return user, 201
 
     def put(self, name):
@@ -58,26 +65,30 @@ class User(Resource):
         parser.add_argument("occupation")
         args = parser.parse_args()
 
-        for user in self.users:
-            if name == user['name']:
-                user['age'] = args['age']
-                user['occupation'] = args['occupation']
-                return user, 200
+        res = users_collection.find({'name': name})
+        if res:
+            user = {
+                'age': args['age'],
+                'occupation': args['occupation']
+            }
+            users_collection.update_one({'name': name}, {'$set': user})
+            return user, 200
 
         user = {
             'name': name,
             'age': args['age'],
             'occupation': args['occupation']
         }
-        self.users.append(user)
 
+        users_collection.insert_one(user)
         return user, 201
 
     def delete(self, name):
-        self.users = [user for user in self.users if user['name'] != name]
+        users_collection.delete_one({'name': name})
         return 'User deleted', 200
 
 
 if __name__ == '__main__':
+    # load_data()
     api.add_resource(User, '/user/<string:name>')
     app.run(debug=True)
